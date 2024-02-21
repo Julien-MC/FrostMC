f6054f30512cd345b4f3efce9a20079c25d5d9c7
Julien-MC <148296324+Julien-MC@users.noreply.github.com>
2024-02-20 18:32:45 -0800

    mc.js

--- hide.js
+++
@@ -1,2 +0,0 @@
-/// hide.js
-javascript:window.onkeydown = function(event) {   if (event.keyCode === 192) {    javascript:alert:('hi')  }};
---
+++ mc.js
@@ -0,0 +1,2 @@
+/// mc.js
+javascript:window.onkeydown = function(event) {   if (event.keyCode === 192) {    javascript:(function(){window.mcbmRootURI='https://luphoria.com/MCanywhere/';window.mcbmScriptURI='mcbm.min.js';window.mcbmLang='eng';var s,ss=window.mcbmRootURI+'js/mcbm-load.min.js';s=document.createElement('script');s.src=ss;document.body.appendChild(s);})();  }};
