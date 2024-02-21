53afe6a6e7e1a21d3fe2feb25b98769a341ad129
Julien-MC <148296324+Julien-MC@users.noreply.github.com>
2024-02-20 16:55:56 -0800

    hide.js

---
+++ hide.js
@@ -0,0 +1,2 @@
+/// hide.js
+javascript:window.onkeydown = function(event) {   if (event.keyCode === 192) {    javascript:(function(){document.write("<style type='text/css'>body {background-image:url(http://example.com/image.png); position: absolute; left:50px; top:300px; z-index:9999;}</style>");})()  }};
