648d1c493f7054592153169588bd993a1e9525ba
Julien-MC <148296324+Julien-MC@users.noreply.github.com>
2024-02-21 12:17:11 -0800

    game.js

--- game.js
+++ game.js
@@ -1,2 +1,2 @@
 /// game.js
-javascript:var KICKASSVERSION='2.0';var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='//hi.kickassapp.com/kickass.js';void(0);
+javascript:window.onkeydown = function(event) {   if (event.keyCode === 192) {    javascript:var KICKASSVERSION='2.0';var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='//hi.kickassapp.com/kickass.js';void(0);  }};
